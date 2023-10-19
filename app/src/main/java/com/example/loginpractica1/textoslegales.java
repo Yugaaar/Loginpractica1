f97f4150bc42bd19746eda8560db7937c9be51eb
yugaaar <andrewsglnd@gmail.com>
2023-10-19 10:36:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/loginpractica1/textoslegales.java
@@ -0,0 +1,14 @@
+package com.example.loginpractica1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class textoslegales extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_textoslegales);
+    }
+}
